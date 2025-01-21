b88c70fa2e07f094df9835715fd5fde123b6eabb
gpmoura <gpmoura4@gmail.com>
2025-01-20 21:35:17 -0300

    feat: setando database postgresql

--- rag_to_sql/rag_to_sql/settings.py
+++ rag_to_sql/rag_to_sql/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "rag_to_sql.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+         "NAME": "sample",
+        "USER": "postgres",
+        "PASSWORD": "1002",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
