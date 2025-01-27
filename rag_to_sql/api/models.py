ad8ccd5070e2725fd859e5bf69947f600df9f2b0
gpmoura <gpmoura4@gmail.com>
2025-01-27 19:40:37 -0300

    feat: configurando models database

--- rag_to_sql/api/models.py
+++ rag_to_sql/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# o que o usuario precisa pra cadastrar o banco
+# senha 
+# link do banco?
+# email do user
+# 
+class Database(models.Model):
+    user = models.CharField(max_length=200)
+
+    db_name = models.CharField(max_length=200)
+
+    db_url_acess = models.CharField(max_length=500)
