5aee3414dfaeb3fb8e79288f782b6ffffe9321e4
LuizBrandi <luizfilipebrandi@gmail.com>
2025-01-31 20:59:53 -0300

    feat: creating rag_service

--- rag_to_sql/api/services/rag_service.py
+++ rag_to_sql/api/services/rag_service.py
@@ -6,14 +6,47 @@ from llama_index.core.workflow import (
     Context,
     Event,
 )
+from llama_index.core import SQLDatabase, VectorStoreIndex
+from llama_index.core.objects import (
+    SQLTableNodeMapping,
+    ObjectIndex,
+    SQLTableSchema,
+)
+from llama_index.core.retrievers import SQLRetriever
+from typing import List
 
 
-class TextToSQLWorkflow1(Workflow):
+class SQLTableRetriever():
+    def __init__(self, engine, table_infos):
+        sql_database = SQLDatabase(engine)
+
+        table_node_mapping = SQLTableNodeMapping(sql_database)
+        table_schema_objs = [
+            SQLTableSchema(table_name=t.table_name, context_str=t.table_summary)
+            for t in table_infos
+        ]  # add a SQLTableSchema for each table
+
+        self.obj_index = ObjectIndex.from_objects(
+            table_schema_objs,
+            table_node_mapping,
+            VectorStoreIndex,
+        )
+
+    def retrieve(self, top_k: int = 3):
+        return self.obj_index.as_retriever(similarity_top_k=top_k)
+
+
+class SQLRetriever():
+    def __init__(self, sql_database):
+        self.sql_retriever = SQLRetriever(sql_database)
+
+
+class TextToSQLWorkflow(Workflow):
     """Text-to-SQL Workflow that does query-time table retrieval."""
 
     def __init__(
         self,
-        obj_retriever,
+        obj_retriever: SQLTableRetriever,
         text2sql_prompt,
         sql_retriever,
         response_synthesis_prompt,
