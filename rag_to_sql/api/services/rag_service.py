93601fca33cac6d932bbb35046e981e63873a2a6
gpmoura <gpmoura4@gmail.com>
2025-02-05 21:01:32 -0300

    ragservice prototipo

--- rag_to_sql/api/services/rag_service.py
+++ rag_to_sql/api/services/rag_service.py
@@ -20,6 +20,19 @@ from llama_index.core.prompts.default_prompts import DEFAULT_TEXT_TO_SQL_PROMPT
 from llama_index.core import PromptTemplate
 from llama_index.core.llms import ChatResponse
 
+from llama_index.core.prompts import ChatPromptTemplate
+from llama_index.core.bridge.pydantic import BaseModel, Field
+from llama_index.llms.openai import OpenAI
+from llama_index.core.llms import ChatMessage
+
+import os
+import openai
+
+from dotenv import load_dotenv
+load_dotenv()
+
+openai.api_key = os.getenv["OPENAI_API_KEY"]
+
 
 class PromptFactory:
     @staticmethod
@@ -35,6 +48,22 @@ class PromptFactory:
             "Query: {query_str}\nSQL: {sql_query}\nSQL Response: {context_str}\nResponse: "
         )
 
+#OPEN_AI CLASS
+class OpenAISQLGenerator():
+    def __init__(self, llm):
+        self.llm = llm
+        # self.prompt = text2sql_prompt
+    
+    def generate(self, context: str, query: str, prompt: str) -> str:
+        fmt_messages = prompt.format_messages(
+            query_str=query, schema=context
+        )
+        return self.llm.chat(fmt_messages)
+
+class LLMFactory:
+    @staticmethod
+    def create_llm(model: str):
+        return OpenAI(model=model)
 
 
 class SQLTableRetriever():
@@ -60,9 +89,10 @@ class SQLTableRetriever():
     #     return self.obj_index.as_retriever(similarity_top_k=top_k)
 
 
-class SQLRetriever():
+# Class que executa as querys no banco
+class SQLRunQuery():
     def __init__(self, sql_database):
-        self.sql_retriever = SQLRetriever(sql_database)
+        self.sql_executor = SQLRetriever(sql_database)
 
 
 class TableRetrieveEvent(Event):
@@ -84,9 +114,9 @@ class TextToSQLWorkflow(Workflow):
         self,
         obj_retriever: SQLTableRetriever,
         text2sql_prompt,
-        sql_retriever,
+        sql_executor: SQLRunQuery,
         response_synthesis_prompt,
-        llm,
+        sql_generator: OpenAISQLGenerator,
         *args,
         **kwargs
     ) -> None:
@@ -94,9 +124,9 @@ class TextToSQLWorkflow(Workflow):
         super().__init__(*args, **kwargs)
         self.obj_retriever = obj_retriever
         self.text2sql_prompt = text2sql_prompt
-        self.sql_retriever = sql_retriever
+        self.sql_executor = sql_executor
         self.response_synthesis_prompt = response_synthesis_prompt
-        self.llm = llm
+        self.sql_generator = sql_generator
     
     @step
     def retrieve_tables(
@@ -115,9 +145,19 @@ class TextToSQLWorkflow(Workflow):
         self, ctx: Context, ev: TableRetrieveEvent
     ) -> TextToSQLEvent:
         """Generate SQL statement."""
-        fmt_messages = PromptFactory.create_text2sql_prompt(
-            query_str=ev.query, schema=ev.table_context_str
-        )
-        chat_response = self.llm.chat(fmt_messages)
+        chat_response = self.sql_generator.generate(ctx, ev.query, PromptFactory.create_text2sql_prompt())
         sql = parse_response_to_sql(chat_response)
         return TextToSQLEvent(sql=sql, query=ev.query)
+    
+    @step
+    def generate_response(self, ctx: Context, ev: TextToSQLEvent) -> StopEvent:
+        """Run SQL retrieval and generate response."""
+        #Executar a query no banco
+        retrieved_rows = self.sql_retriever.retrieve(ev.sql)
+        fmt_messages = PromptFactory.create_response_synthesis_prompt(
+            sql_query=ev.sql,
+            context_str=str(retrieved_rows),
+            query_str=ev.query,
+        )
+        chat_response = llm.chat(fmt_messages)
+        return StopEvent(result=chat_response)
