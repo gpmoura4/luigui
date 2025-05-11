b4089a68ac877b950ca03262073b9fccab87fec1
LuizBrandi <luizfilipebrandi@gmail.com>
2025-05-11 15:22:48 -0300

    updating libs

--- src/core/settings.py
+++ src/core/settings.py
@@ -46,14 +46,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',  # Enable the inner home (home)
+    'api', 
     "rest_framework",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -150,3 +152,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #############################################################
 #############################################################
+
+
+CORS_ALLOW_ALL_ORIGINS = True
