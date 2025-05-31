068ea76d0e3f33793dae2c22b56074439906f338
gpmoura <gpmoura4@gmail.com>
2025-05-31 16:22:24 -0300

    feat: models n migrations for dbaccess

--- src/api/models.py
+++ src/api/models.py
@@ -14,6 +14,7 @@ class UserProfile(models.Model):
     
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='employee')
+    accessible_databases = models.ManyToManyField('Database', related_name='accessible_users', blank=True)
     
     def __str__(self):
         return f"{self.user.username} - {self.role}"
